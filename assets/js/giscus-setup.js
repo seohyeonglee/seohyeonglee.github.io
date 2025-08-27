78c39d9213a1f6783ecfd3f8805f8cf9676325ec
seohyeonglee <seohyeong.lee13@gmail.com>
2025-08-27 16:07:14 +0000

    Deploying to gh-pages from @ seohyeonglee/seohyeonglee.github.io@b1eaabcb05453680e3023afc760d5bbfabd4a9ce 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "seohyeonglee13/seohyeonglee.github.io",
+    "data-repo": "seohyeonglee/seohyeonglee.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
